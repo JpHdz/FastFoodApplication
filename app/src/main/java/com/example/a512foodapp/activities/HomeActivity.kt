0c2fe14e6208d0dfb75cb29a57e6e41416546074
Juan Frausto <juanfrausto@MacBook-Air-de-Juan-2.local>
2024-08-29 18:26:42 -0600

    Recycler view implemented

--- app/src/main/java/com/example/a512foodapp/activities/HomeActivity.kt
+++ app/src/main/java/com/example/a512foodapp/activities/HomeActivity.kt
@@ -9,13 +9,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.a512foodapp.MainActivity
 import com.example.a512foodapp.R
+import com.example.a512foodapp.adapters.CategoryAdapter
+import com.example.a512foodapp.adapters.FoodAdapter
+import com.example.a512foodapp.adapters.RestaurantAdapter
+import com.example.a512foodapp.models.Category
+import com.example.a512foodapp.models.Food
+import com.example.a512foodapp.models.Restaurant
 import com.example.a512foodapp.models.User
 
 class HomeActivity : AppCompatActivity() {
     lateinit var usernameTV : TextView
     lateinit var logoutBtn : ImageView
+    lateinit var categoryRecyclerView : RecyclerView
+    lateinit var restaurantRecyclerView: RecyclerView
+    lateinit var foodRecyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,6 +36,18 @@ class HomeActivity : AppCompatActivity() {
         val sharedPreferences = getSharedPreferences("myPrefs", MODE_PRIVATE)
         usernameTV = findViewById(R.id.usernameTV)
         logoutBtn = findViewById(R.id.logoutBtn)
+        categoryRecyclerView = findViewById(R.id.category_recycleview)
+        restaurantRecyclerView = findViewById(R.id.restaurants_recyclerview)
+        foodRecyclerView = findViewById(R.id.best_food_recycleview)
+        categoryRecyclerView.adapter = CategoryAdapter(Category.categories)
+        categoryRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+
+        restaurantRecyclerView.adapter = RestaurantAdapter(Restaurant.restaurants)
+        restaurantRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+
+        foodRecyclerView.adapter = FoodAdapter(Food.foods)
+        foodRecyclerView.layoutManager = GridLayoutManager(this,2)
+
         val user = User.users[1]
         usernameTV.text = user.computedName
         logoutBtn.setOnClickListener {
---
+++ app/src/main/java/com/example/a512foodapp/adapters/RestaurantAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.a512foodapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.a512foodapp.R
+import com.example.a512foodapp.models.Restaurant
+import com.squareup.picasso.Picasso
+
+class RestaurantAdapter(val restaurantList : List<Restaurant>): RecyclerView.Adapter<RestaurantViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.restaurant_item,parent,false)
+        return RestaurantViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return restaurantList.count()
+    }
+
+    override fun onBindViewHolder(holder: RestaurantViewHolder, position: Int) {
+        val restaurant = restaurantList[position]
+        holder.restaurantName.text = restaurant.name
+        Picasso.get().load(restaurant.image).into(holder.restaurantImage)
+    }
+
+}
+
+class RestaurantViewHolder(view: View) : ViewHolder(view){
+    val restaurantName : TextView = view.findViewById(R.id.restaurant_name)
+    val restaurantImage : ImageView = view.findViewById(R.id.restaurant_image)
+}
